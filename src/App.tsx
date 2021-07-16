1ec5fdd3d2aa24297de80cac916d2ae3d5ebf576
Eduardo Rerick <eduardorerick@gmail.com>
2021-07-15 22:58:16 -0300

    fim do visual do modal utilizando props para styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal'
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    return setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    return setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
-    <GlobalStyle />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal} 
+      />
+      <GlobalStyle />
     </>
   );
 }
